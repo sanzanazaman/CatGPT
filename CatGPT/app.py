724314813b3071289eb71f75b0b16852b0f6ad2f
Sanzana Zaman <70680215+sanzanazaman@users.noreply.github.com>
2025-03-30 09:09:18 -0400

    app.py

--- CatGPT/app.py
+++ CatGPT/app.py
@@ -76,7 +76,7 @@ st.markdown("""
 <p style='color:white; text-align:center; font-size:18px; margin: 0 auto; max-width: 500px;'>
   Well? Can I help you?
 </p>
-<img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjFpcGcxZndnb2xydmh3d3Q0MmJlNThnMjBleGFjOGN1ajJ2YWMxbyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YJ85eVpdZDy7e/giphy.gif"", unsafe_allow_html=True)
+<img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjFpcGcxZndnb2xydmh3d3Q0MmJlNThnMjBleGFjOGN1ajJ2YWMxbyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YJ85eVpdZDy7e/giphy.gif", unsafe_allow_html=True)
 
 # Input field
 user_input = st.text_input("Type something", placeholder="e.g., How are you?", label_visibility="visible")
